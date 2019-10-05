47a427252023b97e7fad9e6f08888a6f5c95103c
christine-vaug <christinev@nevada.unr.edu>
2019-10-04 21:33:33 -0700

    Update bullets.lua

--- game/bullets.lua
+++ game/bullets.lua
@@ -13,9 +13,10 @@ bulletR = {} --the radius of each bullet
 bulletDX = {} --the x component of the direction each bullet will move
 bulletDY = {} --the y component of the direction each bullet will move
 
-timeToNewBullet = 1 --number of frames before a new bullet is generated
+timeToNewBullet = 20 --number of frames before a new bullet is generated
 newBulletTimer = timeToNewBullet
 numberOfBullets = 0 --number of bullets on screen
+bulletSpeedScalar = 100 --how fast the bullets move
 
 bulletRenderInfo = {0, 0, 1} --holds x, y, and radius to send to render
 
@@ -23,8 +24,12 @@ function Bullets:load(r)
 	table.insert(bulletX, love.math.random(1,width)) --the x-coordinate of the bullet
 	table.insert(bulletY, love.math.random(1,height)) --the y-coordinate of the bullet
 	table.insert(bulletR, r) --the radius of the bullet
-	table.insert(bulletDX, 1) --the x component of the direction the bullet will move
-	table.insert(bulletDY, 1) --the y component of the direction the bullet will move
+	sign = love.math.random(0,1)
+	if(sign == 0) then sign = -1 end --randomly determine x direction of bullet
+	table.insert(bulletDX, bulletSpeedScalar * sign * love.math.random( )) --the x component of the direction the bullet will move
+	sign = love.math.random(0,1)
+	if(sign == 0) then sign = -1 end --randomly determine y direction of bullet
+	table.insert(bulletDY, bulletSpeedScalar * sign * love.math.random( )) --the y component of the direction the bullet will move
 end
 
 function Bullets:update(dt)
@@ -36,10 +41,10 @@ function Bullets:update(dt)
 			numberOfBullets = numberOfBullets + 1 --increment number of bullets
 		end
 		for i, v in ipairs(bulletX) do
-			v = v + bulletDX[i] * dt --update the bullet's x coordinate
+			bulletX[i] = bulletX[i] + bulletDX[i] * dt --update the bullet's x coordinate
 		end
 		for i, v in ipairs(bulletY) do
-			v = v + bulletDY[i]	* dt --update the bullet's x coordinate
+			bulletY[i] = bulletY[i] + bulletDY[i] * dt --update the bullet's y coordinate
 		end
 	end
 end
@@ -50,4 +55,11 @@ function Bullets:render(bulletX, bulletY, bulletR)
 		love.graphics.circle("fill", bulletX[i], bulletY[i], bulletR[i])
 	end
 end
-	
+
+function Bullets:spawnCoords()
+	change = love.math.random(0,1) --determine if we configure x or y first
+	xCoord = love.math.random(-1, width + 1)
+	yCoord = love.math.random(-1, height + 1)
+	if(change == 0) then --configure x first
+		if(xCoord > -1 and xCoord < width + 1) then
+			yCoord
