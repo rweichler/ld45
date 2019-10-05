167dbb755725457e8529a0d6b8c360acfe11653e
christine-vaug <christinev@nevada.unr.edu>
2019-10-04 18:25:40 -0700

    stuff

--- game/main.lua
+++ game/main.lua
@@ -3,7 +3,7 @@ by Christine Vaughan and Reed Weichler
 10/4/19]]
 
 require 'controls'
-require 'world'
+-- require 'world'
 require 'console'
 
 function love.update(dt)
--- game/world.lua
+++ game/world.lua
@@ -2,19 +2,35 @@
 by Christine Vaughan and Reed Weichler
 10/4/19]]
 
+allBullets = {}
+
 bullet = {}
 bullet.x = love.math.random(0,love.graphics.getMaxImageSize( ))
 bullet.y = love.math.random(0,love.graphics.getMaxImageSize( ))
 bullet.radius = 5
+bullet.directionX = love.math.random()
+bullet.directionY = love.math.random()
 
-player{}
-player.color = black
+timeToNewBullet = 5
+numberOfBullets = 0
+
+playerColor = black
 
 function love.update(dt)
-	while(player.color == black) --player is black
-		function love.draw()
-			love.graphics.circle("fill", bullet.x, bullet.y, bullet.radius)
-		end
+	if(playerColor == black) -- while player is black
+		timeToNewBullet = timeToNewBullet - 1
+		if(timeToNewBullet == 0)
+			timeToNewBullet = 5
+			function love.draw()
+				love.graphics.circle("fill", bullet.x, bullet.y, bullet.radius)
+			end
+			allBullets[numberOfBullets] = bullet
+			numberOfBullets = numberOfBullets + 1
+			bullet.x = love.math.random(0,love.graphics.getMaxImageSize( ))
+			bullet.y = love.math.random(0,love.graphics.getMaxImageSize( ))
+			bullet.directionX = love.math.random()
+			bullet.directionY = love.math.random()
+		
 
 function bulletMove(direction)
 	
